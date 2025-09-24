6ef15d835b82373d2bfc826ea53b588f129c80d8
Teimur Gasanov <teymurgg321@gmail.com>
2025-09-24 12:44:44 +0600

    feat: remove wasm

--- packages/benchmark/tsup.config.ts
+++ packages/benchmark/tsup.config.ts
@@ -1,5 +1,3 @@
-import { copyFileSync, existsSync } from "node:fs";
-import { join } from "node:path";
 import { defineConfig } from "tsup";
 
 export default defineConfig({
@@ -10,24 +8,4 @@ export default defineConfig({
 	sourcemap: true,
 	clean: true,
 	treeshake: true,
-	onSuccess: async () => {
-		// Copy Rust WASM files
-		const rustWasmSrc = join(
-			__dirname,
-			"./node_modules/@blazediff/wasm/pkg/blazediff_wasm_bg.wasm",
-		);
-		const rustWasmDest = join(__dirname, "dist/blazediff_wasm_bg.wasm");
-		if (existsSync(rustWasmSrc)) {
-			copyFileSync(rustWasmSrc, rustWasmDest);
-		}
-
-		const rustJsSrc = join(
-			__dirname,
-			"./node_modules/@blazediff/wasm/pkg/blazediff_wasm.js",
-		);
-		const rustJsDest = join(__dirname, "dist/blazediff_wasm.js");
-		if (existsSync(rustJsSrc)) {
-			copyFileSync(rustJsSrc, rustJsDest);
-		}
-	},
 });
