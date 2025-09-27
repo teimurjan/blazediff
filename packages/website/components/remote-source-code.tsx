82ec8a35805797c091bb469f8f0345c227a0381b
Teimur Gasanov <teymurgg321@gmail.com>
2025-09-27 18:59:13 +0600

    feat: improve website docs with interactive examples

---
+++ packages/website/components/remote-source-code.tsx
@@ -0,0 +1,34 @@
+"use server";
+
+// @ts-expect-error
+import { compileMdx } from "nextra/compile";
+// @ts-expect-error
+import { MDXRemote } from "nextra/mdx-remote";
+
+interface RemoteSourceCodeProps {
+  url: string;
+  language?: string;
+}
+
+const RemoteSourceCode = async ({ url, language }: RemoteSourceCodeProps) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    return null;
+  }
+  const text = await response.text();
+  const code = text.trim();
+
+  const compiledSource = code
+    ? await compileMdx(
+        `\`\`\`${language} filename="${url}"\n${code}\`\`\``.trim()
+      )
+    : undefined;
+
+  if (!compiledSource) {
+    return null;
+  }
+
+  return <MDXRemote compiledSource={compiledSource} />;
+};
+
+export default RemoteSourceCode;
