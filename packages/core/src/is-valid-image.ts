690bb7efaab986369478f7e9029c049057d17505
Teimur Gasanov <teymurgg321@gmail.com>
2026-01-02 23:53:41 +0600

    chore: get back to single file due to perf issues on linux

--- packages/core/src/is-valid-image.ts
+++
@@ -1,7 +0,0 @@
-import type { Image } from "./types";
-
-/** Check if array is valid pixel data */
-export function isValidImage(arr: unknown): arr is Image["data"] {
-	// work around instanceof Uint8Array not working properly in some Jest environments
-	return ArrayBuffer.isView(arr) && (arr as any).BYTES_PER_ELEMENT === 1;
-}
