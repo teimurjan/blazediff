a4588730e3823d4a2a71a03c5e0d4b43092722bc
Teimur Gasanov <teymurgg321@gmail.com>
2026-02-19 21:03:01 +0600

    docs: fix fixtures paths

--- packages/ui/stories/fixtures.ts
+++ packages/ui/stories/fixtures.ts
@@ -1,8 +1,8 @@
 export const FIXTURE_1_A =
-	"https://raw.githubusercontent.com/teimurjan/blazediff/refs/heads/main/apps/image-benchmark/fixtures/blazediff/1a.png";
+	"https://raw.githubusercontent.com/teimurjan/blazediff/refs/heads/main/fixtures/blazediff/1a.png";
 export const FIXTURE_1_B =
-	"https://raw.githubusercontent.com/teimurjan/blazediff/refs/heads/main/apps/image-benchmark/fixtures/blazediff/1b.png";
+	"https://raw.githubusercontent.com/teimurjan/blazediff/refs/heads/main/fixtures/blazediff/1b.png";
 export const FIXTURE_2_A =
-	"https://raw.githubusercontent.com/teimurjan/blazediff/refs/heads/main/apps/image-benchmark/fixtures/blazediff/2a.png";
+	"https://raw.githubusercontent.com/teimurjan/blazediff/refs/heads/main/fixtures/blazediff/2a.png";
 export const FIXTURE_2_B =
-	"https://raw.githubusercontent.com/teimurjan/blazediff/refs/heads/main/apps/image-benchmark/fixtures/blazediff/2b.png";
+	"https://raw.githubusercontent.com/teimurjan/blazediff/refs/heads/main/fixtures/blazediff/2b.png";
